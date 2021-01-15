d304b5a253e20ac6621dac7b7e13aef4640d715c
Pontus Fagerström <pontus.fagerstrom@gu.se>
2021-01-15 10:07:09 +0100

    argparse added

--- Kaltura/apptokens/README.md
+++ Kaltura/apptokens/README.md
@@ -1,13 +1,10 @@
-# add,remove,list kaltura apptokens
-read comments in code for usage.  
-basicly: 
-  * Add KMC role
-  * list roles with API to get roleid
-  * add apptoken with a description and the roleid
-  * list apptokens to get tokenid and token
-  * use tokenid and token to make API calls
+## helper for kaltura apptokens
+  * list KMC roles
+  * list apptokens
+  * add apptoken
+  * delete apptoken
 
-# install and configure  
+## install and configure  
 Install Python3 *[https://www.python.org/downloads/](https://www.python.org/downloads/)*  
 Install KalturaApiClient library `pip3 install KalturaApiClient`  
 Read *[https://developer.kaltura.com/api-docs/VPaaS-API-Getting-Started/application-tokens.html)* on how to create apptoken  
--- Kaltura/apptokens/apptokens.py
+++ Kaltura/apptokens/apptokens.py
@@ -1,73 +1,120 @@
-import json,time,hashlib,sys
-from datetime import datetime,timedelta
-from KalturaClient import *
-from KalturaClient.Plugins.Core import *
+""" Helper for kaltura apptokens """
 
-def add_token(desc,priv):
-    appToken = KalturaAppToken()
-    appToken.description = desc
-    appToken.hashType = KalturaAppTokenHashType.SHA256
-    appToken.sessionType = KalturaSessionType.ADMIN
-    appToken.sessionPrivileges = "setrole:"+priv
-    client.appToken.add(appToken)
+import argparse
+from datetime import datetime
+from KalturaClient import KalturaConfiguration, KalturaClient
+from KalturaClient.Plugins.Core import (
+    KalturaAppTokenFilter,
+    KalturaAppToken,
+    KalturaUserRoleFilter,
+    KalturaFilterPager,
+    KalturaAppTokenHashType,
+    KalturaSessionType,
+)
+from KalturaClient.exceptions import KalturaException
 
-def list_apptoken(from_date):
-    filter =  	KalturaAppTokenFilter()
-    filter.createdAtGreaterThanOrEqual = from_date
-    appToken = KalturaAppToken()
-    result = client.appToken.list(filter)
-    count=result.totalCount
-    print('total apptokens created after '+datetime.fromtimestamp(from_date).strftime('%c')+': '+str(count))
-    time.sleep(3)
-    nid=0
-    while nid < count:
-        for i in result.objects:
-            skapad=datetime.fromtimestamp(i.createdAt).strftime('%c')
-            print(skapad)
-            print(i.description)
-            print(i.token)
-            print(i.id)
-            nid=nid +1
 
-def delete_apptoken(id):
-    delete=client.appToken.delete(id)
+def kaltura_session():
+    """ get ks from user """
+    ks = input("Enter ks: ")
+    config = KalturaConfiguration()
+    config.serviceUrl = "https://api.kaltura.nordu.net/"
+    client = KalturaClient(config)
+    client.clientConfiguration["clientTag"] = "appToken-helper"
+    client.setKs(ks)
+    return client
 
-# SESSION CONFIG
-kaltura_partner_id = ""
-kaltura_admin_secret = ""
-kaltura_admin_userid = ""
-config = KalturaConfiguration()
-config.serviceUrl = "https://api.kaltura.nordu.net/"
-client = KalturaClient(config)
-ks = client.session.start(
-	kaltura_admin_secret,
-	kaltura_admin_userid,
-	KalturaSessionType.ADMIN,
-	kaltura_partner_id)
-client.setKs(ks)
 
-# LIST APPTOKENS
-# uncomment to list apptokens created from a certain datetime
-#list_apptoken(datetime(2020, 6, 19, 0, 0).timestamp())
+def list_app_tokens(args):
+    """ list appTokens """
+    client = kaltura_session()
 
-# ADD APPTOKEN
-# uncomment to add apptoken
-#apptoken_description="description of apptoken"
-#kmcroleid="123456789"
-#add_token(apptoken_description,kmcroleid)
+    if args.apptoken:
+        filter = KalturaAppTokenFilter()
+        if args.created_at:
+            createdAtFilter = datetime(
+                args.created_at[0], args.created_at[1], args.created_at[2], 0, 0
+            ).timestamp()
+            filter.createdAtGreaterThanOrEqual = createdAtFilter
+        result = client.appToken.list(filter)
+        count = result.totalCount
+        print("totalcount returned: " + str(count) + "\n")
 
-# DELETE APPTOKEN
-# uncomment to delete apptoken
-#apptokenid=""
-#delete_apptoken(apptokenid)
+        nid = 0
+        while nid < count:
+            for i in result.objects:
+                print("id: " + i.id)
+                print("token: " + i.token)
+                print(i.description)
+                print(
+                    "created at: "
+                    + datetime.fromtimestamp(i.createdAt).strftime("%c")
+                    + "\n"
+                )
+                nid = nid + 1
+    elif args.kmcroles:
+        filter = KalturaUserRoleFilter()
+        pager = KalturaFilterPager()
+        pager.pageIndex = 1
+        pager.pageSize = 500
+        listroles = client.userRole.list(filter, pager)
+        for i in listroles.objects:
+            print(i.name + " " + str(i.id))
 
-# LIST KMC ROLES
-# uncomment to list your KMC roles to get kmcroleid
-#filter=KalturaUserRoleFilter()
-#pager=KalturaFilterPager()
-#pager.pageIndex=1
-#pager.pageSize=500
-#listroles=client.userRole.list(filter,pager)
-#print(listroles)
-#for i in listroles.objects:
-#    print(i.name+" "+str(i.id))
+
+def delete_app_token(args):
+    """ delete appToken """
+    client = kaltura_session()
+
+    client.appToken.delete(args.apptokenid)
+    try:
+        client.appToken.get(args.apptokenid)
+    except KalturaException as error:
+        if error.code == "APP_TOKEN_ID_NOT_FOUND":
+            print("appToken deleted")
+
+
+def add_app_token(args):
+    """ add appToken """
+    client = kaltura_session()
+    apptoken = KalturaAppToken()
+    apptoken.description = args.desc
+    apptoken.hashType = KalturaAppTokenHashType.SHA256
+    apptoken.sessionType = KalturaSessionType.ADMIN
+    apptoken.sessionPrivileges = "setrole:" + args.kmcrole
+    result = client.appToken.add(apptoken)
+    print("New appToken created at: " + str(datetime.fromtimestamp(result.createdAt)))
+    print("id: " + result.id)
+    print("token: " + result.token)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Helper for Kaltura apptokens")
+    subparsers = parser.add_subparsers(help="sub-command help")
+    parser_add = subparsers.add_parser("add", help="add appToken")
+    parser_add.add_argument("desc", help="appToken description", type=str)
+    parser_add.add_argument("kmcrole", help="appToken description", type=str)
+    parser_add.set_defaults(func=add_app_token)
+    parser_list = subparsers.add_parser("list", help="list appTokens or KMC roles")
+    parser_list.add_argument(
+        "-a", "--apptoken", help="list appTokens", action="store_true"
+    )
+    parser_list.add_argument(
+        "-c",
+        "--created-at",
+        type=int,
+        help="appTokens created after",
+        nargs=3,
+        metavar=("YYYY", "MM", "DD"),
+    )
+    parser_list.add_argument(
+        "-k", "--kmcroles", help="list KMC roles", action="store_true"
+    )
+    parser_list.set_defaults(func=list_app_tokens)
+    parser_delete = subparsers.add_parser("delete", help="delete appToken")
+    parser_delete.add_argument("apptokenid", help="appToken id")
+    parser_delete.set_defaults(func=delete_app_token)
+    args = parser.parse_args()
+
+    args.func(args)
